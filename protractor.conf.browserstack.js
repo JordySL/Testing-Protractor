bcc7ccb0105d6b163cda1ae3fef6b70243bd5a3c
BRAINSHARK\nsullivan <nsullivan@brainshark.com>
2017-09-25 11:13:25 -0400

    #noissue: Test results output

--- protractor.conf.browserstack.js
+++ protractor.conf.browserstack.js
@@ -2,31 +2,36 @@
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
 const { SpecReporter } = require('jasmine-spec-reporter');
+const { JUnitXmlReporter } = require('jasmine-reporters');
 
 exports.config = {
-  allScriptsTimeout: 60000,
-  specs: [
-    './e2e/**/*.e2e-spec.ts'
-  ],
-  browserstackUser: 'nolansullivan1',
-  browserstackKey: 'RvdDJJpxS1PY8iDzziwu',
-  capabilities: {
-    'browserName': 'chrome'
-  },
-  params: {
-	baseUrl: 'https://wwwqa.brainshark.com/'
-  },
-  baseUrl: 'https://wwwqa.brainshark.com/',
-  framework: 'jasmine',
-  jasmineNodeOpts: {
-    showColors: true,
-    defaultTimeoutInterval: 30000,
-    print: function() {}
-  },
-  onPrepare() {
-    require('ts-node').register({
-      project: 'e2e/tsconfig.e2e.json'
-    });
-    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
-  }
+	allScriptsTimeout: 60000,
+	specs: [
+		'./e2e/**/*.e2e-spec.ts'
+	],
+	browserstackUser: 'nolansullivan1',
+	browserstackKey: 'RvdDJJpxS1PY8iDzziwu',
+	capabilities: {
+		'browserName': 'chrome'
+	},
+	params: {
+		baseUrl: 'https://wwwqa.brainshark.com/'
+	},
+	baseUrl: 'https://wwwqa.brainshark.com/',
+	framework: 'jasmine',
+	jasmineNodeOpts: {
+		showColors: true,
+		defaultTimeoutInterval: 30000,
+		print: function () { }
+	},
+	onPrepare() {
+		require('ts-node').register({
+			project: 'e2e/tsconfig.e2e.json'
+		});
+		jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+
+		jasmine.getEnv().addReporter(
+			new JUnitXmlReporter('test-results/JUnitXML/', true, true)
+		);
+	}
 };
--- protractor.conf.local.js
+++ protractor.conf.local.js
@@ -2,30 +2,35 @@
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
 const { SpecReporter } = require('jasmine-spec-reporter');
+const { JUnitXmlReporter } = require('jasmine-reporters');
 
 exports.config = {
-  allScriptsTimeout: 60000,
-  specs: [
-    './e2e/**/*.e2e-spec.ts'
-  ],
-  capabilities: {
-    'browserName': 'chrome'
-  },
-  params: {
-	baseUrl: 'https://nsullivan-t460.hq.brainshark.com/'
-  },
-  directConnect: true,
-  baseUrl: 'https://nsullivan-t460.hq.brainshark.com/',
-  framework: 'jasmine',
-  jasmineNodeOpts: {
-    showColors: true,
-    defaultTimeoutInterval: 30000,
-    print: function() {}
-  },
-  onPrepare() {
-    require('ts-node').register({
-      project: 'e2e/tsconfig.e2e.json'
-    });
-    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
-  }
+	allScriptsTimeout: 60000,
+	specs: [
+		'./e2e/**/*.e2e-spec.ts'
+	],
+	capabilities: {
+		'browserName': 'chrome'
+	},
+	params: {
+		baseUrl: 'https://nsullivan-t460.hq.brainshark.com/'
+	},
+	directConnect: true,
+	baseUrl: 'https://nsullivan-t460.hq.brainshark.com/',
+	framework: 'jasmine',
+	jasmineNodeOpts: {
+		showColors: true,
+		defaultTimeoutInterval: 30000,
+		print: function () { }
+	},
+	onPrepare() {
+		require('ts-node').register({
+			project: 'e2e/tsconfig.e2e.json'
+		});
+		jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+
+		jasmine.getEnv().addReporter(
+			new JUnitXmlReporter('test-results/JUnitXML/', true, true)
+		);
+	}
 };
