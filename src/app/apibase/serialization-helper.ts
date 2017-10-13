93599eab0a5f2355bf30ef41678e4c63dc4b834b
Felipe Jaramillo <felipe.jaramillo@yuxiglobal.com>
2017-10-13 14:03:52 -0500

    back to normal

---
+++ src/app/apibase/serialization-helper.ts
@@ -0,0 +1,14 @@
+export class SerializationHelper {
+    static toInstance<T>(obj: T, jsonObj: any) : T {
+        if (typeof obj["fromJSON"] === "function") {
+            obj["fromJSON"](jsonObj);
+        }
+        else {
+            for (var propName in jsonObj) {
+                obj[propName] = jsonObj[propName]
+            }
+        }
+
+        return obj;
+    }
+}
