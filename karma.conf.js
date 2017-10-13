fe968276a44233977685a0270c68e815119877b3
Felipe Jaramillo <felipe.jaramillo@yuxiglobal.com>
2017-10-13 13:38:51 -0500

    karma conf back to normal

--- karma.conf.js
+++ karma.conf.js
@@ -4,46 +4,33 @@
 module.exports = function (config) {
   config.set({
     basePath: './',
-    frameworks: ['jasmine', 'karma-typescript'],
+    frameworks: ['jasmine', '@angular/cli'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('karma-phantomjs-launcher'),
-      require('karma-typescript'),
-    ],
-    baseUrl: 'https://wwwqa.brainshark.com/',
-    client: {
+	    require('@angular/cli/plugins/karma'),
+	    require('karma-phantomjs-launcher')
+	],
+	baseUrl: 'https://wwwqa.brainshark.com/',
+    client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      reports: ['html', 'lcovonly'],
+      reports: [ 'html', 'lcovonly' ],
       fixWebpackSourcePaths: true
-    },
-    files: [{
-      pattern: 'e2e/**/*api.spec.ts',
-      included: true
-    }],
+	},
+	files: [
+		{pattern: 'e2e/**/*api.spec.ts', included: false},
+		{pattern: 'src/**/*api.spec.ts', included: false},
+		'src/test.ts'
+	  ],
     angularCli: {
       environment: 'dev'
-    },
-    karmaTypescriptConfig: {
-      compilerOptions: {
-        emitDecoratorMetadata: true,
-        experimentalDecorators: true,
-        jsx: "react",
-        module: "commonjs",
-        sourceMap: true,
-        target: "ES6"
-      },
-      exclude: ["node_modules"]
-    },
-    preprocessors: {
-      "e2e/**/*.ts": ["karma-typescript"], // *.tsx for React Jsx
-    },
-    browserNoActivityTimeout: 60000,
-    reporters: ['progress', 'kjhtml', 'karma-typescript'],
+	},
+	browserNoActivityTimeout: 60000,
+    reporters: ['progress', 'kjhtml'],
     //trxReporter: { outputFile: 'test-results.trx', shortTestName: false },
     port: 9876,
     colors: true,
