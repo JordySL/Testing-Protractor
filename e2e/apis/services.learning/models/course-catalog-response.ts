e5180a384163d33ab15d204798dadca3539069b1
Smith Asencios <sasencios@brainshark.com>
2018-01-08 19:37:38 +0000

    course catalog test

---
+++ e2e/apis/services.learning/models/course-catalog-response.ts
@@ -0,0 +1,33 @@
+export class CourseCatalogResponse {
+	public TotalRows: number;
+	public Filters: Filters[];
+	public Items:Items[];
+	
+	constructor() { }
+}
+
+export class Filters {
+	public FilterID: number;
+	public FilterType: number;
+	public Name: string;
+	public Details: Details[];
+
+	constructor() { }
+}
+
+export class Details {
+	public Count: number;
+	public Description: string;
+	public FullPath: string;
+	public Id: number;
+	SubFilters: Details[]
+
+	constructor() { }
+}
+
+export class Items {
+	public Id: number;
+	public IsCourse: boolean;
+	public IsCurriculum: boolean;
+	constructor() { }
+}
